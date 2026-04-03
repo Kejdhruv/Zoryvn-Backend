1679f00477a5582799c3719635f0fb49fb0f6d3a
Dhruv <dhruvnkejriwal@gmail.com>
2026-04-03 13:59:27 +0530

    User SignUp Complete

--- Backend/server.js
+++ Backend/server.js
@@ -2,19 +2,27 @@ import express from "express";
 import cors from "cors";
 import cookieParser from "cookie-parser";
 import dotenv from "dotenv";
-
 dotenv.config();
+
+import Authentication from "./Routes/User/Auth.js"
+
+
 
 const app = express();
 const PORT = 8990;
 
 app.use(cookieParser());
+app.use(express.json());
 
 app.use(cors({
   origin: "*",
   credentials: true
 }));
 
+app.use("/", Authentication);
+
+
 app.listen(PORT, () => {
   console.log(`Server running at http://localhost:${PORT}`);
-});
+}); 
+
